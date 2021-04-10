8c62baf50e5cb3b516e5a33e11cc424027e64a3d
Ryan Culpepper <ryanc@racket-lang.org>
2021-04-11 01:44:06 +0200

    rename library

--- scramble-lib/info.rkt
+++ scramble-lib/info.rkt
@@ -3,10 +3,10 @@
 ;; pkg info
 
 (define version "0.1")
-(define collection "rl")
+(define collection "scramble")
 (define deps '("base"))
 (define pkg-authors '(ryanc))
 
 ;; collection info
 
-(define name "rl")
+(define name "scramble")
