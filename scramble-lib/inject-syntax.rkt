f6705ef50a5ba94dbc7d43b70e87112430187457
Ryan Culpepper <ryanc@racket-lang.org>
2021-04-11 14:21:50 +0200

    added inject-syntax

---
+++ scramble-lib/inject-syntax.rkt
@@ -0,0 +1,31 @@
+;; Copyright 2020 Ryan Culpepper
+;; Licensed under the Apache License, Version 2.0
+
+#lang racket/base
+(require (for-syntax racket/base))
+(provide begin/inject-syntax)
+
+(define-syntax (begin/inject-syntax stx)
+  (syntax-case stx ()
+    [(_ . body)
+     (let ()
+       (define (bad msg) (raise-syntax-error #f msg stx #'e))
+       ;; The following code is almost the same as syntax-local-eval, but we
+       ;; must remove the intdef scope from the resulting syntax, so that
+       ;; computed definitions and requires work, for example.
+       (define name (car (generate-temporaries '(begin/inject-syntax))))
+       (define intdef (syntax-local-make-definition-context))
+       (syntax-local-bind-syntaxes (list name) #'(lambda () . body) intdef)
+       (define results
+         (let ([name* (internal-definition-context-introduce intdef name)])
+           (call-with-values (syntax-local-value name* #f intdef) list)))
+       (cond [(and (pair? results) (null? (cdr results)))
+              (define result (car results))
+              (unless (syntax? result)
+                (bad (format "result was not syntax\n  result: ~e" result)))
+              (syntax-local-introduce
+               (internal-definition-context-introduce intdef result 'remove))]
+             [else
+              (bad (format "result arity mismatch~a\n  expected: 1\n  received: ~s"
+                           ";\n expected number of values not received"
+                           (length results)))]))]))
---
+++ scramble/tests/inject-syntax.rkt
@@ -0,0 +1,60 @@
+#lang racket/base
+(require (for-syntax racket/base)
+         rackunit
+         scramble/inject-syntax
+         racket/list)
+
+;; ----
+
+(define-for-syntax do-require? #t)
+
+(begin/inject-syntax
+  (if do-require? #'(require racket/string) #'(begin)))
+
+(define (my-string-join xs sep)
+  (error "using my string-join!"))
+
+(begin/inject-syntax
+  (if (identifier-binding #'string-join)
+      #'(begin)
+      #'(define string-join my-string-join)))
+
+(check-equal? (string-join '("hello" "world!") " ")
+              "hello world!")
+
+;; ----
+
+(define (my-string-exclaim str)
+  (regexp-replace* #rx"[.]" str "!"))
+
+(begin/inject-syntax
+  (if (identifier-binding #'string-exclaim)
+      #'(begin)
+      #'(define string-exclaim my-string-exclaim)))
+
+(check-equal? (string-exclaim "Hello. Nice to see you.")
+              "Hello! Nice to see you!")
+
+;; ----
+
+(module slow-popcount racket/base
+  (provide slow-popcount)
+  (define (slow-popcount n)
+    (cond [(zero? n) 0]
+          [else (+ (slow-popcount (quotient n 2))
+                   (if (bitwise-bit-set? n 0) 1 0))])))
+(require (for-syntax 'slow-popcount) 'slow-popcount)
+
+(define (faster-popcount n)
+  (define (byte-popcount b)
+    (bytes-ref (begin/inject-syntax
+                 #`(quote #,(apply bytes (for/list ([n 256]) (slow-popcount n)))))
+               b))
+  (cond [(zero? n) 0]
+        [else (+ (faster-popcount (arithmetic-shift n -8))
+                 (byte-popcount (bitwise-bit-field n 0 8)))]))
+
+(for ([i 100])
+  (define n (random #e1e6))
+  (check-equal? (faster-popcount n)
+                (slow-popcount n)))
