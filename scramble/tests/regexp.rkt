4f736f82229710270977cc61a09b04bfb7b4abe8
Ryan Culpepper <ryanc@racket-lang.org>
2022-05-01 12:36:06 +0200

    update tests for regexp change (avoid posix classes)

--- scramble/tests/regexp.rkt
+++ scramble/tests/regexp.rkt
@@ -5,6 +5,8 @@
 (require rackunit
          scramble/regexp)
 
+(define use-posix-classes? #f)
+
 (define-RE as (* "a"))
 (define-RE bbs (* "bb"))
 
@@ -20,12 +22,19 @@
 (check-equal? (px ^ (report "abc")) #px"^(abc)")
 (check-equal? (px (report (cat "abc" (or "d" $)))) #px"(abc(?:d|$))")
 (check-equal? (px (mode "i" "abc")) #px"(?i:abc)")
-(check-equal? (px (test (look (or "a" "bc")) (* (chars alpha)))) #px"(?(?=a|bc)[[:alpha:]]*)")
+(if use-posix-classes?
+    (check-equal? (px (test (look (or "a" "bc")) (* (chars alpha)))) #px"(?(?=a|bc)[[:alpha:]]*)")
+    (check-equal? (px (test (look (or "a" "bc")) (* (chars alpha)))) #px"(?(?=a|bc)[A-Za-z]*)"))
 (check-equal? (px (unicode "Ll")) #px"\\p{Ll}")
 (check-equal? (px (unicode (not "Ll"))) #px"\\P{Ll}")
 (check-equal? (px "abc") #px"abc")
 (check-equal? (px "[abc]*") #px"\\[abc\\]\\*")
 
-(check-equal? (px (chars [#\A #\Z])) #px"[[:upper:]]")
-(check-equal? (px (chars [#\A #\Z] "rst")) #px"[[:upper:]r-t]")
-(check-equal? (px (chars upper lower)) #px"[[:alpha:]]")
+(cond [use-posix-classes?
+       (check-equal? (px (chars [#\A #\Z])) #px"[[:upper:]]")
+       (check-equal? (px (chars [#\A #\Z] "rst")) #px"[A-Zr-t]")
+       (check-equal? (px (chars upper lower)) #px"[[:alpha:]]")]
+      [else
+       (check-equal? (px (chars [#\A #\Z])) #px"[A-Z]")
+       (check-equal? (px (chars [#\A #\Z] "rst")) #px"[A-Zr-t]")
+       (check-equal? (px (chars upper lower)) #px"[A-Za-z]")])
