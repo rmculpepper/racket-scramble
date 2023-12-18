907d3fd9b73abd5c998f1e7613b4fa4f9097b272
Ryan Culpepper <ryanc@racket-lang.org>
2023-12-18 14:12:21 -0500

    ... numbers

--- scramble-lib/number.rkt
+++ scramble-lib/number.rkt
@@ -1,4 +1,4 @@
-;; Copyright 2020-2021 Ryan Culpepper
+;; Copyright 2020-2023 Ryan Culpepper
 ;; SPDX-License-Identifier: Apache-2.0
 
 #lang racket/base
@@ -21,7 +21,31 @@
            (-> exact-integer? exact-positive-integer? exact-integer?)]
           [floor-multiple
            (-> exact-integer? exact-positive-integer? exact-integer?)]
-          ))
+          [string->integer
+           (->* [string?]
+                [#:radix (integer-in 2 16) #:min real? #:max real?]
+                (or/c exact-integer? #f))]
+          )
+
+         MIN-INT16
+         MAX-INT16
+         MAX-UINT16
+
+         MIN-INT32
+         MAX-INT32
+         MAX-UINT32
+
+         MIN-INT64
+         MAX-INT64
+         MAX-UINT64
+
+         int16?
+         int32?
+         int64?
+
+         uint16?
+         uint32?
+         uint64?)
 
 (define exact inexact->exact)
 (define inexact exact->inexact)
@@ -76,3 +100,36 @@
 (define (floor-multiple n d)
   #;(* (quotient n d) d)
   (- n (modulo n d)))
+
+;; ----------------------------------------
+;; String conversion
+
+(define (string->integer s
+                         #:radix [radix 10]
+                         #:min [minimum -inf.0]
+                         #:max [maximum +inf.0])
+  (define n (string->number s radix))
+  (and n (<= minimum n maximum) n))
+
+;; ----------------------------------------
+;; Limits
+
+(define MIN-INT16 (- (expt 2 15)))
+(define MAX-INT16 (sub1 (expt 2 15)))
+(define MAX-UINT16 (sub1 (expt 2 16)))
+
+(define MIN-INT32 (- (expt 2 31)))
+(define MAX-INT32 (sub1 (expt 2 31)))
+(define MAX-UINT32 (sub1 (expt 2 32)))
+
+(define MIN-INT64 (- (expt 2 63)))
+(define MAX-INT64 (sub1 (expt 2 63)))
+(define MAX-INT64 (sub1 (expt 2 63)))
+
+(define (int16? v) (and (exact-integer? v) (<= MIN-INT16 v MAX-INT16)))
+(define (int32? v) (and (exact-integer? v) (<= MIN-INT32 v MAX-INT32)))
+(define (int64? v) (and (exact-integer? v) (<= MIN-INT64 v MAX-INT64)))
+
+(define (uint16? v) (and (exact-integer? v) (<= 0 v MAX-UINT16)))
+(define (uint32? v) (and (exact-integer? v) (<= 0 v MAX-UINT32)))
+(define (uint64? v) (and (exact-integer? v) (<= 0 v MAX-UINT64)))
